4d16ce61e7dfd6f3e78603832e74959519984f2d
gasparmnz <rm94580@estudante.fieb.edu.br>
2025-08-27 17:34:57 -0300

    Add files via upload

---
+++ Clean Energy/app/routes/router.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.render('pages/home');
+});
+router.get('/home', function(req, res) {
+    res.render('pages/home');
+});
+
+
+
+module.exports = router;
