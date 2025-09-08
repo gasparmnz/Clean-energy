84f8816def4df170051cebf70be5fcb64b85c2b5
Matheus Gaspar Menezes <rm94580@estudante.fieb.edu.br>
2025-09-08 15:30:56 -0300

    ajuste do router para por mais produtos e header

--- app/routes/router.js
+++ app/routes/router.js
@@ -25,11 +25,62 @@ const produtos = [
     local: "Socorro - SP",
     imagem: "cana.png",
     descricao: "Bagaço de cana fresco, ideal para geração de energia limpa."
-  }
+  },
+  {
+    id: 4,
+    nome: "Serragem",
+    preco: 350,
+    local: "Sorocaba - SP",
+    imagem: "serragem.png",
+    descricao: "Serragem seca e uniforme, perfeita para briquetes e geração de energia biomassa."
+  },
+  {
+    id: 5,
+    nome: "Resto de colheita",
+    preco: 380,
+    local: "Santo André - SP",
+    imagem: "restodecolheita.png",
+    descricao: "Resto de colheita rico em biomassa, excelente para compostagem e produção de energia renovável."
+  },
+  {
+    id: 6,
+    nome: "Pallets",
+    preco: 1000,
+    local: "Ribeirão Preto - SP",
+    imagem: "pellets.png",
+    descricao: "Pallets de madeira reutilizáveis, ideais para soluções sustentáveis."
+  },
+];
+
+const produtos2 = [
+  {
+    id: 101,
+    nome: "Resíduos de café",
+    preco: 650,
+    local: "RJ",
+    imagem: "residuosdecafe.png",
+    descricao: "Resíduos de café reciclados, ideais para produção de energia limpa e fertilização do solo."
+  },
+  {
+    id: 102,
+    nome: "palhademilho",
+    preco: 250,
+    local: "São Paulo - SP",
+    imagem: "palhademilho.png",
+    descricao: "Palha de milho seca e versátil, ideal para ração, cobertura do solo e produção de energia renovável."
+  },
+  {
+    id: 103,
+    nome: "Lenha tratada",
+    preco: 250,
+    local: "Campinas - SP",
+    imagem: "Lenha.png",
+    descricao: "Lenha tratada com baixa umidade, ideal para geração de energia."
+  },
 ];
 
 router.get('/', function(req, res) {
-  res.render('pages/produtos', { produtos });
+  res.render('pages/produtos', { produtos, produtos2 });
 });
 
 router.get('/home', function(req, res) {
@@ -38,7 +89,7 @@ router.get('/home', function(req, res) {
 
 router.get('/item/:id', function(req, res) {
   const id = parseInt(req.params.id);
-  const produto = produtos.find(p => p.id === id);
+  const produto = [...produtos, ...produtos2].find(p => p.id === id);
 
   if (!produto) {
     return res.status(404).send("Produto não encontrado");
@@ -46,5 +97,6 @@ router.get('/item/:id', function(req, res) {
 
   res.render('pages/item', { produto });
 });
+
 
 module.exports = router;
