976cd49b7ee3f1990f0434344599f280c45ce793
Henrique <henrique.evangelista08@gmail.com>
2026-07-01 07:25:46 -0300

    Revert "autenticador e app"
    This reverts commit 84a0fe8cdbc3472999e3a056f93447eae37a274e.

--- app.js
+++ app.js
@@ -19,9 +19,26 @@ app.use(session({
   saveUninitialized: true
 }));
 
-const { attachUser } = require('./middleware/autenticadorMiddleware');
-
-app.use(attachUser);
+// Middleware global: injeta usuário na sessão em res.locals
+// Assim todas as views (header, sidebar) têm acesso a `usuario`
+app.use((req, res, next) => {
+  if (req.session.userId) {
+    res.locals.usuario = {
+      id: req.session.userId,
+      nome: req.session.nomeUsuario,
+      email: req.session.emailUsuario,
+      perfil: req.session.perfil,       // 'comprador' | 'vendedor'
+      Tipo: req.session.tipo,           // 'PF' | 'PJ'
+      tipo: req.session.tipo,
+      Nome: req.session.nomeUsuario,
+      Email: req.session.emailUsuario,
+       foto: req.session.fotoUsuario
+    };
+  } else {
+    res.locals.usuario = null;
+  }
+  next();
+});
 
 const rotas = require('./app/routes/router');
 const rotasAdm = require('./app/routes/router-adm');
